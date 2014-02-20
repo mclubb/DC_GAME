72f9efe66abc8a53fcfa0e9100d42c5f36bf1a60
Mike Clubb <mclubb@charter.net>
2014-02-20 16:03:59 -0600

    Update GameSurfaceView.java
    Added on touch event

--- src/com/stlwd/dc_game/GameSurfaceView.java
+++ src/com/stlwd/dc_game/GameSurfaceView.java
@@ -6,15 +6,34 @@ import android.opengl.GLSurfaceView;
 public class GameSurfaceView extends GLSurfaceView {
 
 	GameRenderer mRenderer;
+	float mPreviousMouseX, mPreviousMouseY;
 	
 	public GameSurfaceView(Context context) {
 		super(context);
+		
+		mPreviousMouseX = 0;
+		mPreviousMouseY = 0;
 		
 		mRenderer = new GameRenderer(context);
 		
 		setEGLContextClientVersion(2);
 		
 		setRenderer(mRenderer);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		float x = e.getX();
+		float y = e.getY();
+		
+		switch(e.getAction()) {
+			case MotionEvent.ACTION_MOVE:
+				
+				break;
+		}
+		
+		mPreviousMouseX = x;
+		mPreviousMouseY = y;
 	}
 
 }
