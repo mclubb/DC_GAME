bfefdb89fb7127330951ca2d3a6eaea7acc8f3e6
Mike Clubb <mclubb@charter.net>
2014-04-21 00:16:14 -0500

    Added CardType and some other properties

--- src/com/stlwd/dc_game/Card.java
+++ src/com/stlwd/dc_game/Card.java
@@ -2,8 +2,26 @@ package com.stlwd.dc_game;
 
 import android.content.Context;
 
+public enum CardType
+{
+	VILLAIN,
+	SUPER_VILLAIN,
+	HERO,
+	EQUIPMENT, 
+	SUPER_POWER,
+	LOCATION,
+	STARTER,
+	WEAKNESS
+}
+
 public class Card extends Sprite{
 	boolean active = false;
+	
+	String title;
+	String description; // Don't know if I will go into this much detail
+	CardType type;
+	int victoryPoints;
+	int cost;
 	
 	public Card(Context context, int ResourceID, float x, float y, float z, float h, float w, float d, float rot)
 	{
